6688b7157aeccd4d0f0f95c8360ac6d13b0ae2a2
ashutosh sharma <btta98@hotmail.com>
2015-11-26 01:54:10 +1300

    Update

---
+++ Avatar Customisation/Assets/Boy/UnityChan/Scripts/IdleChanger.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Animator))]
+
+
+
+public class IdleChanger : MonoBehaviour
+{
+	
+	private Animator anim;						// 
+	private AnimatorStateInfo currentState;		// 
+	private AnimatorStateInfo previousState;	// 
+	public bool _random = false;				//
+	public float _threshold = 0.5f;				// 
+	public float _interval = 2f;				// 
+	//private float _seed = 0.0f;					// 
+	
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		//
+		anim = GetComponent<Animator> ();
+		currentState = anim.GetCurrentAnimatorStateInfo (0);
+		previousState = currentState;
+		// 
+		StartCoroutine ("RandomChange");
+	}
+	
+	// Update is called once per frame
+	void  Update ()
+	{
+		// 
+		if (Input.GetKeyDown ("up") || Input.GetButton ("Jump")) {
+			//next 
+			anim.SetBool ("Next", true);
+		}
+		
+		// 
+				if (Input.GetKeyDown ("down")) {
+			// 
+			anim.SetBool ("Back", true);
+		}
+		
+		// 
+		if (anim.GetBool ("Next")) {
+			// 
+			currentState = anim.GetCurrentAnimatorStateInfo (0);
+			if (previousState.nameHash != currentState.nameHash) {
+				anim.SetBool ("Next", false);
+				previousState = currentState;				
+			}
+		}
+		
+		// 
+		if (anim.GetBool ("Back")) {
+			// 
+			currentState = anim.GetCurrentAnimatorStateInfo (0);
+			if (previousState.nameHash != currentState.nameHash) {
+				anim.SetBool ("Back", false);
+				previousState = currentState;
+			}
+		}
+	}
+
+
+	void OnGUI()
+	{
+				GUI.Box(new Rect(Screen.width - 110 , 10 ,100 ,90), "Change Motion");
+				if(GUI.Button(new Rect(Screen.width - 100 , 40 ,80, 20), "Next"))
+					anim.SetBool ("Next", true);
+				if(GUI.Button(new Rect(Screen.width - 100 , 70 ,80, 20), "Back"))
+					anim.SetBool ("Back", true);
+	}
+
+
+	// 
+	IEnumerator RandomChange ()
+	{
+		// 
+		while (true) {
+			//
+			if (_random) {
+				// 
+				float _seed = Random.Range (-1f, 1f);
+				if (_seed <= -_threshold) {
+					anim.SetBool ("Back", true);
+				} else if (_seed >= _threshold) {
+					anim.SetBool ("Next", true);
+				}
+			}
+			// 
+			yield return new WaitForSeconds (_interval);
+		}
+
+	}
+
+}
